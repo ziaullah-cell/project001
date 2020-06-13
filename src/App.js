5b14b373dc58e0053e08573b30edbde8a5281f7d
zia ullah <ziabukhari47@gmail.com>
2020-06-13 08:53:07 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [isDay,setDay] = useState(true);
+  let [pvalue,nvalue] = useState(1);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`isDay ${isDay?"isDay":"isNight"}`}>
+      <h2>Developed by Zia Ullah Student of PIAIC-AI Batch 35</h2>
+     Good = {isDay?"Day":"Night"}
+     <br /> Counter Value ={pvalue}
+    
+    <button onClick={()=>setDay(!isDay)}>click for D/N</button>
+    <button onClick={()=>nvalue(pvalue+2)}>Update Counter</button>
+   
     </div>
   );
 }
 
 export default App;
+
